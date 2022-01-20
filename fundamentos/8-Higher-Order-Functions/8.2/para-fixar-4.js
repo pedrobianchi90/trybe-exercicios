2852a561492619c309637296df48c7948407153d
Pedro Bianchi <pedroedm@gmail.com>
2022-01-20 06:49:29 -0300

    Aula 8.2

---
+++ fundamentos/8-Higher-Order-Functions/8.2/para-fixar-4.js
@@ -0,0 +1,13 @@
+const people = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+  ];
+  
+  // Adicione se código aqui
+  people.sort((person1, person2) => person1.age - person2.age);
+  people.sort((person1, person2) => person2.age - person1.age);
+
+  console.log(people);
